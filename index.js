3a6416f9671ed821c5f91145c46c8e07e93ee0ae
Jonathan Redwine <jredwine1@gmail.com>
2024-07-02 10:10:03 -0400

    Made actual code change to hopefully trigger startup

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ const init = () => {
   app.use('/auth', authRouter);
 
   app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Free Lunch yo, but a new version!' });
+    res.json({ message: 'PLEASE WORK HOLY FUCK' });
   });
 
   startServer(app);
